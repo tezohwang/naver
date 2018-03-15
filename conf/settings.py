8576b37956ac42b4eef65d33dc3cb513dc48642f
tezohwang <tony.hwang@wizpace.com>
2018-03-15 11:50:54 +0900

    ready to publish

--- conf/settings.py
+++ conf/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8)2)!k8(+4s1vca%rx03f)+mb)c%qkcaq#=_b8%w6#ou_yuvd0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.125.58.51']
 
 
 # Application definition
